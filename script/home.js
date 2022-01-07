2cd682db1f8551835d23da299e35ef7df53849f0
Bastien <bastien.patureau@yahoo.com>
2022-01-07 12:10:33 +0100

    hide and show list books details

--- script/home.js
+++ script/home.js
@@ -7,6 +7,12 @@ axios.get(`http://localhost/prep-compet/api.php`)
 .then(response => {
   console.log(response.data.records)
   response.data.records.forEach(e => {
-    list_books.innerHTML += `<li data-id="${e.id}" class='list-book-item'> ${e.title} </li>`
+    list_books.innerHTML += `<li data-id="${e.id}" class='list-book-item hide-bot'> <h2 class="titre">${e.title}</h2> <div><p>author: ${e.authors}</p><p>publisher: ${e.publisher}</p> <p>how much pages: ${e.pageCount}</p><p>description: ${e.description} </p></div></li>`
   });
+})
+
+
+list_books.addEventListener("click", e => {
+  e.preventDefault() 
+  e.target.parentNode.classList.toggle("hide-bot")
 })
